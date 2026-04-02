b942ea1ccb9171bb90636a8527d6f6e004a8583a
Anu5156 <anukuacharya@gmail.com>
2026-04-02 23:29:22 +0530

    added string_formatting file

---
+++ PYTHON/string_formatting.py
@@ -0,0 +1,26 @@
+#string formatting allows us to create DYNAMIC STRINGS keys combining (plugging in) values an variables in them
+
+#it is also used in print statement where we pass string
+# there are 3 types of formatting string method
+#1.Formatting with % operator
+#2.Formatting with format() method
+#3.f-string
+
+
+#1.Formatting with % operator
+s1="Mayank"
+print("my name is: %s"%(s1))
+#output:my name is: Mayank
+
+#2.Formatting with format() method
+
+
+
+
+
+#ex1:
+a="HELLO"
+b="WORLD"
+print(f"")
+
+
