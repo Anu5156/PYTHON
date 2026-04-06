93874a4e6b24abb304aa63f02659b48f664351bd
Anu5156 <anukuacharya@gmail.com>
2026-04-06 21:31:20 +0530

    changed the name from conditional to control statement and also added if_else_statement

---
+++ PYTHON/if_else_statement.py
@@ -0,0 +1,40 @@
+#If -else statement evaluates the test expression execute if block,when condition is True execute else block if condition is false
+#Indentation is used to separate 
+
+"""syntax:
+if(expression):
+    statement 1
+    statement 2
+else:
+    statement 3
+    statement 4
+    """
+#indentation and colon is very important
+raining=True
+if (raining):
+    print("Go back to home")
+    print("It's Holiday")
+else:
+    print("go to school")
+    print("its not holiday")
+#Go back to home
+# It's Holiday
+
+knowledge=True
+print("Know your knowledge")
+if(knowledge):
+    print("You are fit to know the problem of entire world")
+else:
+    print("you are unfit to know the problem of the entire world")
+# You are fit to know the problem of entire world
+
+#check if the number is less than 10
+num=int(input("enter the number:"))
+if(num<10):
+    print("you have enterd the valid answer")
+else:
+    print("you have not entered the valid answer")
+"""enter the number:-90
+you have enterd the valid answer"""  
+
+
