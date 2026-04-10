3745fc652d974269b1b767d8c9f99e4624617bb7
Anu5156 <anukuacharya@gmail.com>
2026-04-10 15:44:25 +0530

    added data_structure_part1

---
+++ PYTHON/list_Data_structure_part1_intro.py
@@ -0,0 +1,34 @@
+#what is datastructure?
+"""Data structure is a way of organizing and storing data in a computer so that it can be accessed and modified efficiently. It provides a means to manage large amounts of data and perform operations on it effectively. Common data structures include arrays, linked lists, stacks, queues, trees, graphs, and hash tables. Each data structure has its own advantages and disadvantages, and the choice of which one to use depends on the specific requirements of the application being developed."""
+#ex:
+#in built data structure
+#list
+my_list = [1, 2, 3, 4, 5]
+print(my_list)
+
+#tuple
+my_tuple = (1, 2, 3, 4, 5)
+print(my_tuple)
+
+#dictionary
+my_dict = {"name": "Alice", "age": 30, "city": "New York"}
+print(my_dict)
+
+#set
+my_set = {1, 2, 3, 4, 5}
+print(my_set)
+
+#stack
+"""it follows the first In First Out (FIFO) principle, where the first element added to the stack is the first one to be removed. It is commonly used for function calls, expression evaluation, and backtracking algorithms. The main operations of a stack include push (adding an element), pop (removing the top element), and peek (viewing the top element without removing it)."""
+
+#Queue
+"""it follows the First In First Out (FIFO) principle, where the first element added to the queue is the first one to be removed. It is commonly used for task scheduling, buffer management, and breadth-first search algorithms. The main operations of a queue include enqueue (adding an element), dequeue (removing the front element), and front (viewing the front element without removing it)."""
+
+#key kind of data structure
+
+#Tree
+"""A tree is a hierarchical data structure that consists of nodes connected by edges. It is used to represent relationships between data in a parent-child manner. Each node in a tree can have zero or more child nodes, and there is one root node that serves as the starting point of the tree. Trees are commonly used in various applications such as file systems, databases, and decision-making algorithms. Some common types of trees include binary trees, binary search trees, AVL trees, and B-trees."""
+
+#Graph
+"""A graph is a data structure that consists of a set of vertices (also called nodes) and a set of edges that connect pairs of vertices. Graphs can be directed or undirected, depending on whether the edges have a direction associated with them. They are used to represent relationships between data in various applications such as social networks, transportation networks, and computer networks. Common operations on graphs include traversal (e.g., depth-first search and breadth-first search), pathfinding (e.g., Dijkstra's algorithm), and graph coloring."""
+
